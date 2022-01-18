55c888d075b2c823678e151d58f3a858892467b3
mfreire29 <mfreire@bief.com.ar>
2022-01-18 00:06:30 -0300

    Cambios Item

--- src/components/DetalleItemCheckout.js
+++ src/components/DetalleItemCheckout.js
@@ -5,7 +5,7 @@ function DetalleItem( { quantity, pictureUrl, title } ) {
     return (
           
       <div className="col-3 col-sm-4 col-md-4 pb-5">
-          <img src={`http://66.97.46.222/images/${pictureUrl}`} className="img-fluid  animate__animated animate__fadeIn" alt="imagen"/>
+          <img src={`http://66.97.46.222/images/${pictureUrl}`} className="img-fluid img-item animate__animated animate__fadeIn" alt="imagen"/>
           <p className="text-center pt-2 pb-0 m-0">{title }</p>
           <p className="text-center p-0 m-0">{ quantity } { quantity > 1 ? 'unidades' : 'unidad' } </p>
       </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -18,23 +18,34 @@ function Item({id, title, description, price, off, pictureUrl}) {
 
     return (
         <>
-                <div className="col-6 col-sm-6 col-md-3 p-3 pb-3 text-center">
+                <div className="col-xs-12 col-sm-6 col-md-3 p-1 mb-3 pb-3 text-center">
+                    <div className='shadow m-3 p-1 py-3 border contenedor-oferta'>
+                    {
+                        off == 0 || off == null || off == '' ?
+                        ''
+                    :
+                        <div className='oferta shadow'>OFERTA</div>
+                    }
+                    
                     <NavLink to={`/item/`+ id} >
-                        <img src={`http://66.97.46.222/images/${pictureUrl}`} className="img-fluid p-2 animate__animated animate__fadeIn" style={{objectFit: "cover"}} alt={description}/>
+                        <img src={`http://66.97.46.222/images/${pictureUrl}`} className="img-fluid img-item p-2 animate__animated animate__fadeIn" style={{objectFit: "cover"}} alt={description}/>
                     </NavLink>
                     <h5 className="card-title text-center my-0 py-0 pt-1">
                         <NavLink to={`/item/`+ id} className="product-title" >
-                            {title}
+                            {
+                                title.length > 30 ?
+                                    <>{title.substring(0, 40)}...</>
+                                :
+                                    title
+                            }
                         </NavLink>        
                     </h5>
-                    <p className="card-text text-center my-0 py-0">{description}</p>
                     {
                         off == 0 || off == null || off == '' ?
                             <h3 className="card-text text-center mt-2 product-price"><b>${price}.-</b></h3>
                         :
                             <>
-                            <h3 className="card-text text-center mt-2 product-price" style={{textDecoration: "line-through"}}><b>${price}.-</b></h3>
-                            <h3 className="card-text text-center mt-2 product-price"><b>${off}.-</b></h3>
+                            <h3 className="card-text text-center mt-2 product-price"> <span style={{textDecoration: "line-through"}}>${price}</span><b> ${off}.-</b></h3>
                             </>
 
                     }
@@ -42,17 +53,18 @@ function Item({id, title, description, price, off, pictureUrl}) {
                     {
                        isInCart(id).length > 0 ?
                             <div>
-                                <p className="text-dark text-xs"> Tienes { isInCart(id)[0].quantity } en el carrito</p>
                                 {
-                                (isInCart(id)[0].quantity) === 0 ?
+                                    (isInCart(id)[0].quantity) === 0 ?
                                     ''
-                                :
+                                    :
                                     <ItemCount initial={1} onAdd={onAdd} enCarro ={ true } />
                                 }
+                                <p className="text-dark pt-2 text-xs"> Tienes { isInCart(id)[0].quantity } en el carrito</p>
                             </div>
                         :
                             <ItemCount initial={1} enCarro= { false } onAdd={onAdd} />
                     }
+                    </div>
                 </div>
         </>
     )
